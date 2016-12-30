b323264a7d42c1e2f5f4abbdf4ad50946f44fb8b
Rodrigo Zamboni <joao.zamboni@dextra-sw.com>
2016-12-30 15:20:37 -0200

    criacao de estrutura de retorno json

--- cinare-web/src/main/java/com/cinare/controller/HelloController.java
+++ cinare-web/src/main/java/com/cinare/controller/HelloController.java
@@ -1,7 +1,11 @@
 package com.cinare.controller;
 
+import com.cinare.model.Library;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/hello")
@@ -11,6 +15,18 @@ public class HelloController {
     @Path("/jair")
     public Response hello() {
         return Response.ok("Hello Jair").build();
+    }
+
+    @GET
+    @Path("/jairson")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Library jairToJson() {
+
+        Library jair = new Library();
+        jair.setAge("300");
+        jair.setName("Luiz");
+
+        return jair;
     }
 
 }
---
+++ cinare-web/src/main/java/com/cinare/model/Library.java
@@ -0,0 +1,26 @@
+package com.cinare.model;
+public class Library {
+
+    private String name;
+    private String age;
+
+    public boolean someLibraryMethod() {
+        return true;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
