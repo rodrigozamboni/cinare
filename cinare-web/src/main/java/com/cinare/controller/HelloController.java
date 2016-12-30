95570dc275c2d4dadfa62b1ea8db720cb853e2e4
Guilherme Gotardo <gui.gotardo@gmail.com>
2016-12-30 15:04:11 -0200

    build

--- cinare-web/src/main/java/com/cinare/controller/HelloController.java
+++ cinare-web/src/main/java/com/cinare/controller/HelloController.java
@@ -1,11 +1,13 @@
 package com.cinare.controller;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
 @Path("/hello")
 public class HelloController {
 
+    @GET
     @Path("/jair")
     public Response hello() {
         return Response.ok("Hello Jair").build();
